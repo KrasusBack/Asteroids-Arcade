fac9f1adb099d7063bfd6583065a8ee644ff75d6
Valery Chirkov <yvalch@yandex.ru>
2020-03-15 23:31:22 +0300

    Added asteroids sprites

---
+++ Assets/Scripts/AsteroidBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidBehaviour : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+
+        _rigidbody2D.velocity = MathfExtentions.DegreeToVector2(Random.Range(0, 360));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
