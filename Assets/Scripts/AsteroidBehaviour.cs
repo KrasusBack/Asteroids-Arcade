52ed76414600122c2f612f6ad7b1357a63486d25
Valery Chirkov <yvalch@yandex.ru>
2020-03-16 23:48:40 +0300

    Set up basic asteroid behaviour example

--- Assets/Scripts/AsteroidBehaviour.cs
+++ Assets/Scripts/AsteroidBehaviour.cs
@@ -8,11 +8,20 @@ public class AsteroidBehaviour : MonoBehaviour
     private float minSpeed = 1f;
     [SerializeField]
     private float maxSpeed = 5f;
+    [SerializeField]
+    private GameObject[] asteroidPrefabs;
+    [SerializeField]
+    private float asteroidScaleMedium = 0.5f;
+    [SerializeField]
+    private float asteroidScaleSmall = 0.25f;
+
+    public enum AsteroidSize { Large, Medium, Small };
 
     private Vector2 _velocityVector;
     private Rigidbody2D _rigidbody2D;
     private AsteroidColliderBehaviour[] _asteroidColliderBehaviours;
     private float _speed;
+    private AsteroidSize _asteroidSize = AsteroidSize.Large;
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +32,7 @@ public class AsteroidBehaviour : MonoBehaviour
         _speed = Random.Range(minSpeed, maxSpeed);
     }
 
-    private void CheckCollisions ()
+    private void CheckCollisions()
     {
         foreach (AsteroidColliderBehaviour asteroidColliderBehaviour in _asteroidColliderBehaviours)
         {
@@ -40,17 +49,57 @@ public class AsteroidBehaviour : MonoBehaviour
         ScreenWrap.CheckAndWrapAround(_rigidbody2D);
 
         CheckCollisions();
-        Move();   
+        Move();
     }
 
-    private void Move ()
+    private void ShatterAsteroid()
+    {
+        if (_asteroidSize == AsteroidSize.Small) return;
+        SpawnNewAsteroids();
+    }
+
+    private void SpawnNewAsteroids ()
+    {
+        for (var i = 0; i < 2; ++i)
+        {
+            var variation = Random.Range(0, asteroidPrefabs.Length - 1);
+            var newAsteroid = Instantiate(asteroidPrefabs[variation], _rigidbody2D.position, transform.rotation);
+
+            AsteroidSize newAsteroidSize = GetPreviousSize(_asteroidSize);
+            newAsteroid.GetComponent<AsteroidBehaviour>().SetSize(newAsteroidSize);
+        }
+    }
+
+    private void Move()
     {
         var newPos = _rigidbody2D.position + _velocityVector * _speed;
         _rigidbody2D.MovePosition(newPos);
     }
 
-    private void Destroy ()
+    //Returns Small Size if there is no previous
+    private AsteroidSize GetPreviousSize (AsteroidSize size)
     {
+        switch (size)
+        {
+            case AsteroidSize.Large:
+                    return AsteroidSize.Medium;
+            case AsteroidSize.Medium:
+                return AsteroidSize.Small;
+        }
+        return AsteroidSize.Small;
+    }
+
+    public void SetSize(AsteroidSize size)
+    {
+        _asteroidSize = size;
+
+        if (size == AsteroidSize.Medium) { transform.localScale = asteroidScaleMedium*Vector3.one; return; }
+        if (size == AsteroidSize.Small) transform.localScale = asteroidScaleSmall*Vector3.one;
+    }
+
+    private void Destroy()
+    {
+        ShatterAsteroid();
         Destroy(gameObject);
     }
 
