d9d9cb944f15df10b2ada780dcfc72f7b551ec4b
Valery Chirkov <yvalch@yandex.ru>
2020-04-06 07:10:48 +0300

    Did some progress in LivesDisplayUpdater

--- Assets/Scripts/Components/UI/LivesDispayUpdater.cs
+++ Assets/Scripts/Components/UI/LivesDispayUpdater.cs
@@ -22,7 +22,7 @@ public class LivesDispayUpdater : TMPUpdater
         {
             if (childTransform.gameObject.activeSelf)
             {
-                if (spriteWidth == 0) spriteWidth = (childTransform as RectTransform).rect.size.y;
+                if (spriteWidth == 0) spriteWidth = (childTransform as RectTransform).rect.size.y*childTransform.localScale.y;
                 activeObjects++;
                 totalChildren++;
             }
@@ -57,30 +57,37 @@ public class LivesDispayUpdater : TMPUpdater
         //need to add extra if there are not enough children
         { } //fine fore now
 
-        
+
         //--Calculate space for sprites
         //Check is all sprites can fit in the rect
-        var boxWidth = GetComponent<RectTransform>().rect.size.x;
+        var rectTransform = GetComponent<RectTransform>();
+        var boxWidth = rectTransform.rect.size.x*transform.localScale.x;
         var widthRatio = boxWidth / (spriteWidth * activeObjects);
-        Debug.Log("widthRatio = boxWidth / (spriteWidth * activeObjects) " + widthRatio + "=" + boxWidth + "/(+" + spriteWidth + "*" + activeObjects + ")");
+        Debug.Log("widthRatio = boxWidth / (spriteWidth * activeObjects) " + widthRatio + "=" + boxWidth + "/(" + spriteWidth + "*" + activeObjects + ")");
         var startPoint = 0f;
         if (widthRatio <= 1)
         {
             //start right from left border
-            startPoint = transform.position.x - boxWidth / 2 + spriteWidth / 2;
+            startPoint = rectTransform.anchoredPosition.x - (boxWidth - spriteWidth) / 2;
+            Debug.Log("widthRation<=1: startPoint = transform.position.x - (boxWidth - spriteWidth) / 2; " + startPoint + "=" + transform.position.x + "-(" + boxWidth + "-" + spriteWidth + ")");
         }
         else
         {
             //all sprites can fit, normal filling
             widthRatio = 1;
-            startPoint = transform.position.x - spriteWidth * activeObjects / 2 + spriteWidth / 2;
+            startPoint = rectTransform.anchoredPosition.x - spriteWidth * livesCount / 2 + spriteWidth / 2;
+            Debug.Log("widthRation>1: startPoint = rectTransform.anchoredPosition.x - spriteWidth * livesCount / 2 + spriteWidth/2;; " + startPoint + "=" + rectTransform.anchoredPosition.x + "-" + spriteWidth + "*" + livesCount+"/2+"+spriteWidth+"/2");
         }
 
         //place life sprites
-        foreach (Transform childTransform in transform)
+        foreach (RectTransform childTransform in transform)
         {
-            childTransform.position = new Vector3(startPoint, childTransform.position.y, childTransform.position.z);
+            if (!childTransform.gameObject.activeSelf) continue;
+            print(childTransform.gameObject.name);
+            //childTransform.anchoredPosition = new Vector3(startPoint, childTransform.position.y, childTransform.position.z);
+            childTransform.anchoredPosition = new Vector2(startPoint, childTransform.anchoredPosition.y);
             startPoint += spriteWidth * widthRatio;
+            print("sp "+ startPoint);
         }
         
     }
