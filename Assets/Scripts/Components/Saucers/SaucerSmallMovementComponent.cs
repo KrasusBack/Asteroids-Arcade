c3b6405b3e74aa03d4604d26de56f6d69d8b1e8a
Valery Chirkov <yvalch@yandex.ru>
2020-04-12 02:40:51 +0300

    Worked on SaucerSmallMovementComponent obstacles avoider method

--- Assets/Scripts/Components/Saucers/SaucerSmallMovementComponent.cs
+++ Assets/Scripts/Components/Saucers/SaucerSmallMovementComponent.cs
@@ -9,7 +9,7 @@ public class SaucerSmallMovementComponent : MovementConponentBase
     private Transform checkerObjectTransform;
     private Vector2 direction;
 
-    List<Collider2D> collidersNear = new List<Collider2D>();
+    Collider2D[] collidersNear;
     ContactFilter2D contactFilter2D = new ContactFilter2D();
     //появиться там где нет астероида (проверка пригодиться еще и для hyperSpace)
     //задать изначальный вектор
@@ -36,7 +36,10 @@ public class SaucerSmallMovementComponent : MovementConponentBase
         //var setUpCheckerComponent = new ObstaclesCheckerComponent(ChangeDirectionBasedOnNearObject);
         //checkerComponent = setUpCheckerComponent;
 
-        contactFilter2D.SetLayerMask(LayerMask.GetMask(LayerMask.LayerToName(gameObject.layer)));
+        //contactFilter2D.SetLayerMask(LayerMask.GetMask(LayerMask.LayerToName(gameObject.layer)));
+        contactFilter2D.SetLayerMask(LayerMask.GetMask("Player", "Asteroids"));
+        contactFilter2D.useTriggers = true;
+        print("mask " + contactFilter2D.layerMask.value);
 
         var someAngle = 60;
         direction = GetRandomDirection(someAngle);
@@ -53,13 +56,45 @@ public class SaucerSmallMovementComponent : MovementConponentBase
     private void ChangeDirectionBasedOnNearObjects()
     {
         _checkCollider.OverlapCollider(contactFilter2D, collidersNear);
+        /*
+        if (collidersNear.Length == 0) return;
 
-        if (collidersNear.Count == 0) return;
-        print("Colliders near:");
-        foreach (var collider in collidersNear)
+        float minDistance = float.MaxValue;
+        Collider2D nearestCollider = new Collider2D();
+        Vector2[] nearCollidersPositions;// = new Vector2[collidersNear.Length];
+        */
+        //print("Colliders near:");
+
+        //Посмотреть на все коллайдеры вокруг. Выбрать ближайший (от него у будем уклоняться
+        //Далее нужно определить куда. Для этого собираем позиции ближайших коллайдеров и отсекаем направления в которых
+        //они находятся. 
+        //Оверкил: можно составить массив весов, в котором чем ближе объект, тем больше его вес. Так, при возникновении
+        // Ситуации в коророй некуда уклоняться придется выбирать направение в сторону самого дальнего объекта
+        //Оверкил: Также можно просто стрелять в ближайший объект 
+
+        /*
+        //find nearest collider to avoid
+        for (var i = 0; i<collidersNear.Length; i++ )
         {
-            print("--" + collider.name);
+            nearCollidersPositions[i] = collidersNear[i].transform.position;
+            var distance = Vector2.Distance(transform.position, nearCollidersPositions[i]);
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                nearestCollider = collidersNear[i];
+            }
+            //print("--" + collider.name);
         }
+        */
+        /*
+        //что-то сделать с minDistance
+        Vector2 directionToNearestCollider = nearestCollider.transform.position - transform.position;
+        var currentDirectionAngle = Vector2.SignedAngle(direction, Vector2.right);
+        var toNearestColliderDirectionAngle = Vector2.SignedAngle(directionToNearestCollider, Vector2.right);
+        var safeAngle = 15;
+        if (currentDirectionAngle - toNearestColliderDirectionAngle > safeAngle) return;
+        direction = Vector2.Reflect(MathfExtentions.DegreeToVector2(toNearestColliderDirectionAngle), direction);
+        */
     }
 
     private void OnDestroy()
