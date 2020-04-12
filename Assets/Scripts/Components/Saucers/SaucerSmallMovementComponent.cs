c0ba94bac251b223fd674827fb35f58d5e619728
Valery Chirkov <yvalch@yandex.ru>
2020-04-12 17:28:20 +0300

    Progressed in SucerSmallMovementComp. Avoiding almost done

--- Assets/Scripts/Components/Saucers/SaucerSmallMovementComponent.cs
+++ Assets/Scripts/Components/Saucers/SaucerSmallMovementComponent.cs
@@ -6,116 +6,107 @@ public class SaucerSmallMovementComponent : MovementConponentBase
 {
     private Saucer _saucer;
     private CircleCollider2D _checkCollider;
-    private Transform checkerObjectTransform;
-    private Vector2 direction;
-
-    private bool timeToCheck = true;
+    private Transform _checkerObjectTransform;
+    private Vector2 _direction;
+    private float _checkInterval = 0.2f;
+    private bool _timeToCheck = true;
 
     List<Collider2D> collidersNear = new List<Collider2D>();
     ContactFilter2D contactFilter2D = new ContactFilter2D();
-    //появиться там где нет астероида (проверка пригодиться еще и для hyperSpace)
-    //задать изначальный вектор
-    //раз в n времени сменять направление / сменять направление если встречает препятствие на пути (раз в n времени проверки (зависит от мастерства movement)
-    //по хорошему еще и определять свободное пространство
-    //при любом задании направления проверять есть ли препятствие на пути
+
     private void Start()
     {
         _saucer = GetComponent<SaucerSettingsComponent>().GetSettings();
         //create obstacles checker object
-        var checkerObject = new GameObject("obstaclesChecker");
-        checkerObjectTransform = checkerObject.transform;
-        checkerObjectTransform.position = transform.position;
-        checkerObject.tag = tag;
-        checkerObject.layer = gameObject.layer;
-
-        _checkCollider = checkerObject.AddComponent<CircleCollider2D>();
-        _checkCollider.isTrigger = true;
-        //change collider checker radious based on its size and avoiding obstacles mastery 
-        _checkCollider.radius = GetComponent<Renderer>().bounds.max.magnitude + _saucer.AvoidingObstaclesMastery * _saucer.MoveSpeed;
-
-        //checkerObject.AddComponent<ObstaclesCheckerComponent>().SomethingFoundOnTheWay += ChangeDirectionBasedOnNearObjects;
-        //var checkerComponent = checkerObject.AddComponent<ObstaclesCheckerComponent>();
-        //var setUpCheckerComponent = new ObstaclesCheckerComponent(ChangeDirectionBasedOnNearObject);
-        //checkerComponent = setUpCheckerComponent;
-
-        //contactFilter2D.SetLayerMask(LayerMask.GetMask(LayerMask.LayerToName(gameObject.layer)));
-        contactFilter2D.SetLayerMask(LayerMask.GetMask("Player", "Asteroids"));
-        contactFilter2D.useTriggers = true;
-        print("mask " + contactFilter2D.layerMask.value);
+        CreateAndSetUpObstaclesCheckerObject();
 
         var someAngle = 60;
-        direction = GetRandomDirection(someAngle);
+        _direction = GetRandomDirection(someAngle);
 
         StartCoroutine(WaitForTheNextObstaclesCheck());
     }
 
+    private void CreateAndSetUpObstaclesCheckerObject ()
+    {
+        var checkerObject = new GameObject("obstaclesChecker");
+        _checkerObjectTransform = checkerObject.transform;
+        _checkerObjectTransform.position = transform.position;
+        checkerObject.tag = tag;
+        checkerObject.layer = gameObject.layer;
+        _checkCollider = checkerObject.AddComponent<CircleCollider2D>();
+        _checkCollider.isTrigger = true;
+        //change collider checker radious based on its size and avoiding obstacles mastery 
+        _checkCollider.radius = GetComponent<Renderer>().bounds.max.magnitude + _saucer.MoveSpeed;
+        contactFilter2D.SetLayerMask(LayerMask.GetMask("Player", "Asteroids"));
+        contactFilter2D.useTriggers = true;
+    }
+
     private void FixedUpdate()
     {
-        Vector3 perpen = checkerObjectTransform.position;
-        
-        if (timeToCheck)
+        //for debug: perpendicular line end pos
+        Vector3 perpen = _checkerObjectTransform.position;
+
+        if (_timeToCheck)
         {
-            timeToCheck = false;
+            _timeToCheck = false;
             perpen = ChangeDirectionBasedOnNearObjects();
         }
-        MoveKinematicRB(_saucer.MoveSpeed, direction);
-        checkerObjectTransform.position = transform.position;
+        MoveKinematicRB(_saucer.MoveSpeed, _direction);
+        _checkerObjectTransform.position = transform.position;
 
+        /* //Show debug lines
         var multiplier = 8;
         Vector3 endPos = new Vector3(checkerObjectTransform.position.x + direction.x * multiplier, 
                                         checkerObjectTransform.position.y + direction.y * multiplier, 
                                         checkerObjectTransform.position.z);
         Debug.DrawLine(checkerObjectTransform.position, endPos, Color.cyan);
         Debug.DrawLine(checkerObjectTransform.position, perpen, Color.red);
+        */
     }
 
-    private Vector3 ChangeDirectionBasedOnNearObjects()
+    private Vector3 ChangeDirectionBasedOnNearObjects() //<-change back to void
     {
         _checkCollider.OverlapCollider(contactFilter2D, collidersNear);
 
-        if (collidersNear.Count == 0) return checkerObjectTransform.position;
+        if (collidersNear.Count == 0) return _checkerObjectTransform.position;
 
         float minDistance = float.MaxValue;
-        Collider2D nearestCollider = new Collider2D();
-        List<Vector2> nearCollidersPositions = new List<Vector2>();
-
+        Vector3 nearestColliderPos = Vector3.zero;
         //find nearest collider to avoid
         for (var i = 0; i < collidersNear.Count; i++)
         {
-            nearCollidersPositions.Add(collidersNear[i].transform.position);
-            var distance = Vector2.Distance(transform.position, nearCollidersPositions[i]);
+            var closestPos = collidersNear[i].ClosestPoint(_checkerObjectTransform.position);
+            var distance = Vector2.Distance(transform.position, closestPos);
             if (distance < minDistance)
             {
+                nearestColliderPos = closestPos;
                 minDistance = distance;
-                nearestCollider = collidersNear[i];
             }
-            //print("--" + collider.name);
         }
 
-        if (!nearestCollider) return checkerObjectTransform.position;
+        //if (!nearestCollider) return _checkerObjectTransform.position;
 
-        print("nearest collider: " + nearestCollider.name);
+        //print("nearest collider: " + nearestCollider.name);
 
         //что-то сделать с minDistance
-        Vector2 directionToNearestCollider = nearestCollider.transform.position - checkerObjectTransform.position;
-        var currentDirectionAngle = Vector2.SignedAngle(Vector2.right, direction);
+        Vector2 directionToNearestCollider = nearestColliderPos - _checkerObjectTransform.position;
+        var currentDirectionAngle = Vector2.SignedAngle(Vector2.right, _direction);
         var toNearestColliderDirectionAngle = Vector2.SignedAngle(Vector2.right, directionToNearestCollider);
 
-        var safeAngle = 300;
-        //по модулю?
-        print($"{ currentDirectionAngle} - {toNearestColliderDirectionAngle}) > {safeAngle}");
-        if (Mathf.Abs( currentDirectionAngle - toNearestColliderDirectionAngle) > safeAngle) return checkerObjectTransform.position;
+        var safeAngle = 30 + 330 * _saucer.AvoidingObstaclesMastery;
+        //if (Mathf.Abs( currentDirectionAngle - toNearestColliderDirectionAngle) > safeAngle) return checkerObjectTransform.position;
+        if (currentDirectionAngle - toNearestColliderDirectionAngle > safeAngle) return _checkerObjectTransform.position;
 
         var toNearestColliderDirection = MathfExtentions.DegreeToVector2(toNearestColliderDirectionAngle);
-        direction = Vector2.Reflect(direction, Vector2.Perpendicular(toNearestColliderDirection));
-        direction.x = -direction.x;
-        direction.y = -direction.y;
-        //direction = Vector2.Reflect(direction, toNearestColliderDirection);
+        //currently good enough version
+        _direction = -(Vector2.Perpendicular(toNearestColliderDirection).normalized + toNearestColliderDirection.normalized);
+        //or just like this:
+        //_direction = Vector2.Perpendicular(toNearestColliderDirection);
 
         var multiplier = 4;
-        Vector3 endPos = new Vector3(checkerObjectTransform.position.x + toNearestColliderDirection.x * multiplier,
-                                        checkerObjectTransform.position.y + toNearestColliderDirection.y * multiplier,
-                                        checkerObjectTransform.position.z);
+        Vector3 endPos = new Vector3(_checkerObjectTransform.position.x + toNearestColliderDirection.x * multiplier,
+                                        _checkerObjectTransform.position.y + toNearestColliderDirection.y * multiplier,
+                                        _checkerObjectTransform.position.z);
 
         //min time = 0.2f
         return endPos;
@@ -123,16 +114,16 @@ public class SaucerSmallMovementComponent : MovementConponentBase
 
     private IEnumerator WaitForTheNextObstaclesCheck()
     {
-        while(true)
+        while (true)
         {
-            timeToCheck = true;
-            yield return new WaitForSeconds(1.2f - _saucer.AvoidingObstaclesMastery);
+            _timeToCheck = true;
+            yield return new WaitForSeconds(_checkInterval);
         }
     }
 
     private void OnDestroy()
     {
-        Destroy(checkerObjectTransform.gameObject);
+        Destroy(_checkerObjectTransform.gameObject);
     }
 
 }
