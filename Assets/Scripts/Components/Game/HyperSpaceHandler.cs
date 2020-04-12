4cdd003213ee331bfdc0071515c0af9a6170dd7d
Valery Chirkov <yvalch@yandex.ru>
2020-04-13 01:29:16 +0300

    Started to add space occupation check before HyperSpaceHandler teleport

--- Assets/Scripts/Components/Game/HyperSpaceHandler.cs
+++ Assets/Scripts/Components/Game/HyperSpaceHandler.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public sealed class HyperSpaceHandler : MonoBehaviour
 {
     private bool _readyToGoIntoHyperSpace = true;
+    private ContactFilter2D contactFilter2D;
+    private Renderer shipRenderer;
+
+    private void Start()
+    {
+        contactFilter2D.SetLayerMask(LayerMask.GetMask("Enemies", "Asteroids"));
+        contactFilter2D.useTriggers = true;
+        shipRenderer = GameCore.Instance.PlayerShip.GetComponentInChildren<Renderer>();
+    }
 
     public void TravelToHyperSpace()
     {
@@ -25,21 +34,38 @@ public sealed class HyperSpaceHandler : MonoBehaviour
         _readyToGoIntoHyperSpace = true;
     }
 
-    private static void SetExitPositionFromHyperSpace(Transform objTransform)
+    private void SetExitPositionFromHyperSpace(Transform objTransform)
     {
         Vector3 newPos;
 
         if (Random.value > GameCore.Instance.PlayerShipSettings.ChanceToAppearInsideAsteroid)
         {
+            int attempts = 0;
             newPos = ScreenToWorld.GetRandomPositionOnScreen(objTransform.position.z);
+            while (attempts<50)
+            {
+                if (CheckIfNewPosIsNotOccupied(newPos)) break;
+                newPos = ScreenToWorld.GetRandomPositionOnScreen(objTransform.position.z);
+                attempts++;
+            }
+            if (!(attempts<50)) throw new System.Exception("HyperSpaceHandler: failed attempts to find free space");
         }
         else
         {
             var someAsteroid = GameObject.FindWithTag("Asteroids");
-            newPos = (someAsteroid == null) ? ScreenToWorld.GetRandomPositionOnScreen(objTransform.position.z) 
+            newPos = (someAsteroid == null) ? ScreenToWorld.GetRandomPositionOnScreen(objTransform.position.z)
                                             : someAsteroid.transform.position;
         }
 
         objTransform.position = newPos;
-    }  
+    }
+
+    private bool CheckIfNewPosIsNotOccupied(Vector2 somePos)
+    {
+        List<Collider2D> colliders = new List<Collider2D>();
+        Physics2D.OverlapCircle(somePos, shipRenderer.bounds.max.magnitude, contactFilter2D, colliders);
+        if (colliders.Count > 0) return false;
+
+        return true;
+    }
 }
